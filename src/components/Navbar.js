d39f13703d3cfcbbbcfbb5f38470768d724dbdf9
Pratik <pratikpemendra@gmail.com>
2024-02-17 16:43:49 +0530

    Changes in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
 import './Navbar.css'; // Import your CSS file for navbar styling
-import mov1 from '../images/mov1.png';
-import mov2 from '../images/mov2.png';
-import mov3 from '../images/mov3.png';
+import logo from '../images/logo.png';
+import home from './Home';
+
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="container">
-        <Link to="/" className="navbar-brand">
-          <img className="mov1" src={mov1} alt="Speedzzz"/>
-          <img className="mov2" src={mov2} alt="Speedzzz"/>
-          <img className="mov3" src={mov3} alt="Speedzzz"/>
+        <Link to="/home" className="navbar-brand">
+          <img className="logo" src={logo} alt="Speedzzz"/>
         </Link>
         <ul className="navbar-nav">
           <li className="nav-item-1">
